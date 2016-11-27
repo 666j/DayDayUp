a3bb09ea55bff262a858d53008f7adf96535c148
666j <17865195372@163.com>
2016-11-27 23:34:32 +0800

    react native样式学习

---
+++ index.android.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+class PropsTest extends Component{
+	render(){
+		let pic = {
+			uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+		};
+		return(
+			<View>
+				<Image source = {pic} style = {{width:193,height:110}}/>
+				<Text>hello{this.props.name}!</Text>
+			</View>
+		);
+	}
+}
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // 试试去掉父View中的`flex: 1`。
+      // 则父View不再具有尺寸，因此子组件也无法再撑开。
+      // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+      <View style={{flex: 1,flexDirection: 'row'}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+class MyProps extends Component{
+	render(){
+		return(
+			<View Style ={styles.welcome}>
+				<PropsTest name = "6j"/>
+				<Blink text ="hell0"/>
+				<FlexDimensionsBasics/>
+			</View>
+		);
+		
+	}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => MyProps);
